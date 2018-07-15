bacac382ddb4429aa91e8c8bdf449cf071f9b32f
mrromadon <mochamadramadhan92@gmail.com>
2018-07-15 21:37:59 +0700

    change dir

--- Linux_Utility/proxy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-PROXY_ID=10.0.01
-PROXY_PORT=9191
-export https_proxy=http://$PROXY_ID:$PROXY_PORT/
-export http_proxy=http://$PROXY_ID:$PROXY_PORT/
-export ftp_proxy=http://$PROXY_ID:$PROXY_PORT/
-export no_proxy=.domain.com
